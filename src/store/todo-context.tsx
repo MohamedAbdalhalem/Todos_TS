79a59f98e06e5dc4ca625ea27173c1cc2823ca2f
MohamedAbdalhalem <mohamed3ab7alem@gmail.com>
2026-03-31 10:47:11 +0200

    fix

--- src/store/todo-context.tsx
+++ src/store/todo-context.tsx
@@ -1,40 +1,41 @@
-import React, { createContext, useState } from 'react'
-import Todo from '../models/todo'
-
+import React, { createContext, useState } from 'react';
+import Todo from '../models/todo';
 
 type TodosContextData = {
-    items: Todo[],
-    addTodo: (todoText: string) => void,
-    deleteTodo : (todoId : string) => void
-}
+  items: Todo[];
+  addTodo: (todoText: string) => void;
+  deleteTodo: (todoId: string) => void;
+};
+
+type TodosContextProviderProps = {
+  children: React.ReactNode;
+};
 
 export const TodosContext = createContext<TodosContextData>({
-    items: [],
-    addTodo: () => { },
-    deleteTodo : () => {}
-})
+  items: [],
+  addTodo: () => {},
+  deleteTodo: () => {},
+});
 
-const TodosContextProvider: React.FC = (props) => {
- const [todos, setTodos] = useState<Todo[]>([])
-  function handleAddNewTodo(todoText : string) {
-    const newTodo = new Todo(todoText)
-    setTodos((prevTodo)=> [...prevTodo, newTodo]) 
+const TodosContextProvider: React.FC<TodosContextProviderProps> = ({ children }) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function handleAddNewTodo(todoText: string) {
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodo) => [...prevTodo, newTodo]);
   }
-  function handleDeleteTodo(todoId: string) {
-    setTodos(prevTodo => {
-      return prevTodo.filter(todo => todo.id !== todoId)
-    })
-    }
-    const contextValue : TodosContextData = {
-        items: todos,
-        addTodo: handleAddNewTodo,
-        deleteTodo: handleDeleteTodo
-    }
-  return (
-    <TodosContext.Provider value={contextValue}>
-      {props.children}
-    </TodosContext.Provider>
-  )
-}
 
-export default TodosContextProvider
+  function handleDeleteTodo(todoId: string) {
+    setTodos((prevTodo) => prevTodo.filter((todo) => todo.id !== todoId));
+  }
+
+  const contextValue: TodosContextData = {
+    items: todos,
+    addTodo: handleAddNewTodo,
+    deleteTodo: handleDeleteTodo,
+  };
+
+  return <TodosContext.Provider value={contextValue}>{children}</TodosContext.Provider>;
+};
+
+export default TodosContextProvider;
